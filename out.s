771c14f51f891313744d13c3000dc56613d5cd33
AahanMehta21 <mehta295@purdue.edu>
2025-01-28 22:12:36 -0500

    Nested while works as well

--- out.s
+++ out.s
@@ -22,16 +22,33 @@ main:
 	pushq %rbp
 	movq %rsp, %rbp
 	.comm	i,8,8
+	.comm	j,8,8
 	movq $1, %r8
 	movq %r8, i(%rip)
 L1:
 	movq i(%rip), %r8
-	movq $10, %r9
+	movq $4, %r9
 	cmpq %r9, %r8
 jg L2
+	movq $0, %r8
+	movq %r8, j(%rip)
 	movq i(%rip), %r8
 	movq %r8, %rdi
 	call printint
+L3:
+	movq j(%rip), %r8
+	movq i(%rip), %r9
+	cmpq %r9, %r8
+jg L4
+	movq j(%rip), %r8
+	movq %r8, %rdi
+	call printint
+	movq j(%rip), %r8
+	movq $1, %r9
+	addq %r8, %r9
+	movq %r9, j(%rip)
+	jmp L3
+L4:
 	movq i(%rip), %r8
 	movq $1, %r9
 	addq %r8, %r9
