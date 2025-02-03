45ed9f080a73e26708c94f0011724f77ab4664fc
AahanMehta21 <mehta295@purdue.edu>
2025-02-03 00:17:02 -0500

    Updated testing system to have centralized data of testcases present

---
+++ testfiles/testcases.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+tests=(
+  "arithop_test_1" "arithop_test_2" "arithop_test_3" "arithop_test_4"
+  "err_arithop_test_1" "err_arithop_test_2" "err_arithop_test_3"
+  "comparision_test_1" "comparision_test_2"
+  "if_test_1" "if_test_2"
+  "if_else_test_1" "if_else_test_2"
+  "nested_if_test_1"
+  "nested_while_test_1"
+  "while_test_1"
+  "print_arithop_test_1" "print_arithop_test_2"
+  "var_test_1" "var_test_2"
+  "for_test_1" "for_test_2" "nested_loop_test_1" "nested_loop_test_2" "char_test_1"
+)
+
